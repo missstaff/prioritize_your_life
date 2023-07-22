bf6fa3c9af8ac73bb342c0f0438ec834d306208a
missstaff <shawnastaff@gmail.com>
2023-07-21 18:27:21 -0700

    login, logout, signup, reset password functionality, routing using react navigator

--- src/utility/utilities.js
+++ src/utility/utilities.js
@@ -1,16 +1,18 @@
 import Toast from "react-native-toast-message";
 import { getFireApp } from "../../getFireApp";
 import { EMAIL_REGEX, PASSWORD_REGEX } from "./constants";
+import { ERROR_MESSAGES } from "./errorMessages";
 
 
 const firebase = getFireApp();
 
 
-export const displayErrorToast = (errorText1, errorText2) => {
+export const displayToast = (id, type) => {
+    const errorType = ERROR_MESSAGES[id];
     return Toast.show({
-        text1: errorText1,
-        text2: errorText2,
-        type: "error",
+        text1: errorType.errorText1,
+        text2: errorType.errorText2,
+        type: type,
     });
 };
 
@@ -19,7 +21,7 @@ export const logOut = async () => {
         await firebase.auth().signOut();
     } catch (error) {
         console.log(`Error: ${error.message}\n${error.stack}`);
-        displayToast("Failed to log out.", "Please try again.");
+        displayToast("login_failure", "error");
         return;
     }
 };
@@ -28,23 +30,26 @@ export const validateTextInput = (textInput) => {
 
     let isValid = false;
 
-    switch (textInput.type) {
-        case 1:
-            isValid = textInput.condition;
-            break;
-        case 2:
+    switch (textInput.id) {
+        case "invalid_email":
             isValid = EMAIL_REGEX.test(textInput.condition);
             break;
-        case 3:
+        case "invalid_password":
             isValid = PASSWORD_REGEX.test(textInput.condition);
             break;
-        case 4:
+        case "invalid_username":
+            isValid = textInput.condition;
+            break;
+        case "password_required":
+            isValid = PASSWORD_REGEX.test(textInput.condition);
+            break;
+        case "passwords_do_not_match":
             isValid = textInput.condition;
             break;
     };
 
     if (!isValid) {
-        displayErrorToast(textInput.errorText1, textInput.errorText2);
+        displayToast(textInput.id, textInput.type);
     }
 
     textInput?.callback && textInput?.callback(isValid);
