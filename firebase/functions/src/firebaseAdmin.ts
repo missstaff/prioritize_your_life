42722fb44d52a6b76a511f2a3bcad7474344ed89
missstaff <shawnastaff@gmail.com>
2023-07-24 19:47:51 -0700

    Basic backend firebase auth testing using built in assert

---
+++ firebase/functions/src/firebaseAdmin.ts
@@ -0,0 +1,14 @@
+import * as admin from 'firebase-admin';
+const serviceAccount = require("../../services.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://organizer-9cce6-rtdb.firebaseio.com", 
+});
+
+
+export const auth = admin.auth();
+export const database = admin.database();
+export const firestore = admin.firestore();
+export const storage = admin.storage();
+export const messaging = admin.messaging();
