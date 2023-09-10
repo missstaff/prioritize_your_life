43534a05613702a7c0038962a31117a1b731d0da
missstaff <shawnastaff@gmail.com>
2023-09-10 10:43:42 -0700

    render tests

--- __tests__/screens/auth/SignUpScreen.spec.js
+++ __tests__/screens/auth/SignUpScreen.spec.js
@@ -1,4 +1,6 @@
 import renderer from "react-test-renderer";
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
+import { useNavigation } from "@react-navigation/native";
 import SignUpScreen from "../../../src/screens/auth/SignUpScreen";
 
 
@@ -6,6 +8,18 @@ describe("SignUpScreen", () => {
 
   afterAll(() => {
     jest.clearAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const { getByText, getByPlaceholderText } = render(<SignUpScreen />);
+    
+    // Test rendering of components
+    expect(getByText('Submit')).toBeTruthy();
+    expect(getByPlaceholderText('Username')).toBeTruthy();
+    expect(getByPlaceholderText('Email')).toBeTruthy();
+    expect(getByPlaceholderText('Password')).toBeTruthy();
+    expect(getByPlaceholderText('Confirm Password')).toBeTruthy();
+    
   });
 
   it("SignUpScreen should render correctly", () => {
